187c55b8f7b8ac2dc8decd9c2292942ac36e88a1
Sara Castrillón <84990901+SaraCastril1@users.noreply.github.com>
2023-08-22 21:16:41 -0500

    Producer's message

---
+++ Reto2/rabbitMQ-python/Prueba-Hello-World/VM-Producer/send.py
@@ -0,0 +1,18 @@
+#Establish the conecction with the RabbitMQ server
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost')) #We're connected now, to a broker on the local machine
+channel = connection.channel()
+
+#Create the hello Queue
+channel.queue_declare(queue='hello')
+
+#In RabbitMQ a message can never be sent directly to the queue, it always needs to go through an exchange
+#This exchange is special ‒ it allows us to specify exactly to which queue the message should go. 
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+
+connection.close()
